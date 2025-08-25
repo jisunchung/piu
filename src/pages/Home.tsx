9d4f341748c180d477d639f8599b5266c5be3190
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-08-25 17:23:44 +0900

    feat : supabase 연동
    - env 파일 생성해서 api key 저장
    - .gitignore에 env 파일 추가
    - remote/supabase파일 내부에서 supabase client를 생성해서 export 해줌
    - Home 에서 supabase client를 콘솔로 출력하여 확인

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import supabase from "@remote/supabase";
+
 export default function HomePage() {
+  console.log(supabase);
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
       hello my name is piu
