4cc31530f23cb953573160238987c2733438a18f
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-08-25 14:42:07 +0900

    chore : import문 정렬 및 관리 ESLint 설정 추가
    - eslint-plugin-import , eslint-import-resolver-typescript설치
    - import문 관리 및 정렬 규칙 적용
    - import문 그룹별 정렬, 경로 해석, 사용 규칙 등을 통해 코드 일관성과 가독성을 높임

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config([
   globalIgnores(["dist"]),
@@ -19,6 +20,53 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+          alwaysTryTypes: true,
+        },
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          paths: ["src"],
+        },
+      },
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "import/no-dynamic-require": "warn",
+      "import/no-nodejs-modules": "warn",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "type",
+          ],
+          pathGroups: [{ pattern: "@/**", group: "internal" }],
+          pathGroupsExcludedImportTypes: ["type"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+        },
+      ],
     },
   },
   eslintConfigPrettier,
