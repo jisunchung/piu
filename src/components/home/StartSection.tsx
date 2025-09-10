5f2b694dbaf670e74958555bff372627e2bbf705
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-09-10 18:42:21 +0900

    feat : StartSection에 GooeyText 적용 #16

--- src/components/home/StartSection.tsx
+++ src/components/home/StartSection.tsx
@@ -1,3 +1,15 @@
+import Flex from "@shared/Flex";
+import { GooeyText } from "@shared/GooeyText";
+
 export default function StartSection() {
-  return <div>hello my name is piu!</div>;
+  return (
+    <Flex align="center" justify="center" direction="col" className="h-screen">
+      <GooeyText
+        texts={["Hello", "My name is", "piu"]}
+        morphTime={1.5}
+        cooldownTime={0.5}
+        textClassName="font-bold whitespace-nowrap text-blue-300"
+      />
+    </Flex>
+  );
 }
