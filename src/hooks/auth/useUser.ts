07185f4e7de8fde1d6fbf166f773148ae6e771bc
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-09-10 12:58:50 +0900

    feat: redux hook 커스텀 훅, useUser 커스텀 훅 구현
    - Redux Toolkit의 useDispatch와 useSelector를 타입과 함께 미리 설정한 커스텀 훅
    - User의 상태관리 로직을 구현한 커스텀 훅

---
+++ src/hooks/auth/useUser.ts
@@ -0,0 +1,17 @@
+import { useAppDispatch, useAppSelector } from "@store/user/hooks";
+import { clearUser, setUser } from "@store/user/userSlice";
+
+import type { User } from "@models/user";
+
+function useUser() {
+  const user = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
+
+  const handleSetUser = (user: User) => dispatch(setUser(user));
+  const handleClearUser = () => dispatch(clearUser());
+
+  return { user, handleSetUser, handleClearUser };
+}
+
+export default useUser;
