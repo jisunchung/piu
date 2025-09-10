1c70143220cf28107aad7fb23a2d8909627f73c8
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-09-10 18:29:00 +0900

    refactor: ProgressBar 내부에서 Flex component를 사용하여 레이아웃 구성

--- src/components/shared/ProgressBar.tsx
+++ src/components/shared/ProgressBar.tsx
@@ -1,3 +1,5 @@
+import Flex from "./Flex";
+
 interface ProgressBarProps {
   progress: number; // 0 to 100
   trackColor?: string;
@@ -10,7 +12,11 @@ export default function ProgressBar({
   barColor = "bg-blue-300",
 }: ProgressBarProps) {
   return (
-    <div className="fixed bottom-0 left-0 flex w-full items-center justify-center p-10">
+    <Flex
+      align="center"
+      justify="center"
+      className="fixed bottom-0 left-0 w-full p-10"
+    >
       <div
         className={`h-2 basis-1/7 ${trackColor} relative overflow-hidden rounded`}
       >
@@ -19,6 +25,6 @@ export default function ProgressBar({
           style={{ width: `${progress}%` }}
         />
       </div>
-    </div>
+    </Flex>
   );
 }
